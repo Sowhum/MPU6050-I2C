3c51c7e2e70ea18bc4223a3aa1da78d6bde842c4
Soham Naithani <115746521+Sowhum@users.noreply.github.com>
2023-10-29 01:40:20 -0400

    Update main.cpp comments

--- example/main.cpp
+++ example/main.cpp
@@ -10,7 +10,7 @@ GStruct g;
 void setup() {  
 
   Wire.begin(21,22);//SDA SCL pins
-
+//setup functions, for int values refer to MPU6050.cpp
   Serial.begin(9600);
   mpu.pwr_setup();
   mpu.gyro_setup(3);
@@ -39,7 +39,7 @@ void print_gyro(){
 }
 
 void loop() {
-
+//refer to MPU6050.h for int values: 131.0,65.5,32.8,16.4 | 16384.0,8192.0,4096.0,2048.0
   mpu.get_acc(1,&a);
   print_acc();
   delay(1000);
